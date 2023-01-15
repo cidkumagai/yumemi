d7a269be45cf6eb68423f764e62fda9acbbdbd53
cidkumagai <f20aa003@gmail.com>
2023-01-15 16:00:54 +0900

    fix import order #6

--- src/population/populationSlice.ts
+++ src/population/populationSlice.ts
@@ -1,5 +1,6 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+
 import type { RootState } from '../app/store';
 
 type ResponsePrefectureList = {
