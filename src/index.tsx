c034acafc4cb059efc3704fb0bed1a7b84292e35
cidkumagai <f20aa003@gmail.com>
2023-01-15 14:21:06 +0900

    install redux/toolkit #3

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
+import { Provider } from 'react-redux';
+
+import { store } from './app/store';
+
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import './styles/index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
---
+++ src/population/populationSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import type { RootState } from '../app/store';
+
+export const populationSlice = createSlice({
+  name: 'populations',
+  initialState: undefined,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export const selectPopulations = (state: RootState) => state.populations;
+export default populationSlice.reducer;
